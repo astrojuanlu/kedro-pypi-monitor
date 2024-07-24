62182da9bcb44272bf8f22b984478e6d870e87af
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2024-07-24 17:03:49 +0200

    Add basic data processing pipeline

---
+++ src/kedro_pypi_monitor/pipelines/data_processing/nodes.py
@@ -0,0 +1,39 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.19.6
+"""
+
+import polars as pl
+
+TARGET_COLUMNS = [
+    "timestamp",
+    "project_name",
+    "version",
+    "major_version",
+    "type",
+    "installer",
+    "python",
+    "major_python",
+    "implementation",
+    "distro",
+    "system",
+    "cpu",
+]
+
+
+def unnest_data(
+    df: pl.DataFrame, target_columns: list[str] = TARGET_COLUMNS
+) -> pl.DataFrame:
+    df = (
+        df.rename({"project": "project_name"})
+        .unnest("file", "details")
+        .with_columns(
+            pl.col(["version", "python"])
+            .str.split(".")
+            .list.slice(0, 2)
+            .list.join(".")
+            .name.prefix("major_")
+        )
+        .select(target_columns)
+    )
+    return df
---
+++ src/kedro_pypi_monitor/pipelines/data_processing/pipeline.py
@@ -0,0 +1,20 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.19.6
+"""
+
+from kedro.pipeline import Pipeline, pipeline, node
+
+from .nodes import unnest_data
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=unnest_data,
+                inputs="pypi_kedro_raw",
+                outputs="pypi_kedro",
+            )
+        ]
+    )
